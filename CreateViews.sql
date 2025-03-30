48a01411e590a5ee9107b90fa625265dd67c0136
ehanckel <123567381+ehanckel@users.noreply.github.com>
2025-03-30 18:18:24 -0400

    first view

--- CreateViews.sql
+++ CreateViews.sql
@@ -1,3 +1,13 @@
-CREATE VIEW ToppingPopularity AS
-    SELECT TopID
-    FROM PIZZA_TOPPING;
+CREATE OR REPLACE VIEW ToppingPopularity AS
+    SELECT T.TopName,
+           SUM(CASE WHEN PT.IsDouble = 1 THEN 2
+                    WHEN PT.IsDouble = 0 THEN 1
+                    ELSE 0
+           END) AS ToppingCount
+    FROM TOPPING T
+    LEFT JOIN PIZZA_TOPPING PT on T.TopID = PT.TopID
+    GROUP BY T.TopName
+    ORDER BY ToppingCount DESC, TopName ASC;
+
+SELECT *
+FROM ToppingPopularity;
