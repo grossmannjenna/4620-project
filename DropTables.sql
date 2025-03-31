7f3262c549b612f60cba19fcc0a17b481c5292bd
grossmannjenna <grossmannjenna@gmail.com>
2025-03-30 23:55:21 -0400

    fixed issues

--- DropTables.sql
+++ DropTables.sql
@@ -1,31 +1,19 @@
 -- Elle Hanckel: DROP TABLE and DROP VIEW statements
+-- Jenna Grossmann
+-- Drop tables that have foreign key constraints referencing other tables first.
 
-DROP TABLE ordertable;
-
-DROP TABLE pizza;
-
-DROP TABLE baseprice;
-
-DROP TABLE customer;
-
-DROP TABLE pickup;
-
-DROP TABLE delivery;
-
-DROP TABLE dinein;
-
-DROP TABLE discount;
-
-DROP TABLE order_discount;
-
-DROP TABLE pizza_discount;
-
-DROP TABLE topping;
-
-DROP TABLE pizza_topping;
-
+DROP TABLE IF EXISTS pizza_topping;
+DROP TABLE IF EXISTS pizza_discount;
+DROP TABLE IF EXISTS order_discount;
+DROP TABLE IF EXISTS pickup;
+DROP TABLE IF EXISTS delivery;
+DROP TABLE IF EXISTS dinein;
+DROP TABLE IF EXISTS pizza;
+DROP TABLE IF EXISTS ordertable;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS topping;
+DROP TABLE IF EXISTS discount;
+DROP TABLE IF EXISTS baseprice;
 DROP VIEW ToppingPopularity;
-
 DROP VIEW ProfitByPizza;
-
 DROP VIEW ProfitByOrderType;
